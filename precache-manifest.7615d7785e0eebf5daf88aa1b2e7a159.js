4842ff68a0b3e9dff9233f7afc92f4675e569a2b
EC2 Default User <ec2-user@ip-172-31-21-11.ap-northeast-2.compute.internal>
2020-10-31 13:50:08 +0000

    Updates

---
+++ precache-manifest.7615d7785e0eebf5daf88aa1b2e7a159.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ecd420d7a639cd270882471b04a941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2649b9e070c2bf07cbaa",
+    "url": "/static/css/main.3695b997.chunk.css"
+  },
+  {
+    "revision": "4817b83c72e99556f207",
+    "url": "/static/js/2.6182540e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6182540e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2649b9e070c2bf07cbaa",
+    "url": "/static/js/main.14a2d585.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kdanielive/precache-manifest.706915279356321d1d482e92848d6f42.js"
+  "/precache-manifest.7615d7785e0eebf5daf88aa1b2e7a159.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kdanielive/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
